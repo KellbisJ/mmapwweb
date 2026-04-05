3eaf2406e2add837879b1afafa2c01b8bd626a81
Kellbis <kellbisdevsw@gmail.com>
2026-04-05 00:09:36 -0400

    making this code have a sense

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-import { dragNode } from "./lib/drag-node.js";
-import { addCoreNodeToContainer } from "./lib/add-core-node-container.js";
-import { addNodeOptions } from "./lib/add-node-options.js";
+import { NodeCreator, NodeHandler } from "./lib/main.js";
 
 const dropeableZone = document.getElementById("dropeableZone");
-const addNodeBtn = document.getElementById("addNode");
-const initialNode = document.getElementById("initialNode");
-const btnInitialNode = initialNode.querySelector(".btn-node");
-// console.log(btnInitialNode);
-// console.log(btnInitialNode instanceof HTMLElement);
 
-// const nodes = document.querySelectorAll(".node");
+const node = new NodeCreator().createNode("CoreNode", { x: "50%", y: "10%" });
 
-addCoreNodeToContainer(dropeableZone, addNodeBtn);
-dragNode(initialNode, dropeableZone);
-addNodeOptions(btnInitialNode, initialNode);
-
-// dragNodesInitializer(dropeableZone, nodes); // test
+const nodeHandler = new NodeHandler();
+nodeHandler.appendNodeToDragZoneContainer(dropeableZone, node);
+nodeHandler.dragNode(node, dropeableZone);

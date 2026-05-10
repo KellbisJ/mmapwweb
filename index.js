a678762581932b7b20754a41dfdb36ba0c49c6e6
Kellbis <kellbisdevsw@gmail.com>
2026-05-09 23:11:38 -0400

    simple mmap, simple life, hexagon background added, touch event fixed on mobile buttons and other interactions when using DragHandler on elements

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { NodeGraph } from "./lib/NodeGraph.js";
 document.addEventListener("DOMContentLoaded", () => {
   const graph = new NodeGraph("dragZone");
 
+  graph.headerDraggable();
   graph.createNode("Main Idea");
 
   const addBtn = document.getElementById("addNode");
